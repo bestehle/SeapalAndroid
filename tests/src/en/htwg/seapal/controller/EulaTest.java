d414ec5bef6aa57c6de9b3106930c937f7d4c560
bestehle <bestehle@htwg-konstanz.de>
2013-04-09 23:23:32 +0200

    add mockito support

--- tests/src/en/htwg/seapal/controller/EulaTest.java
+++ tests/src/en/htwg/seapal/controller/EulaTest.java
@@ -1,15 +1,20 @@
 package en.htwg.seapal.controller;
 
+import org.mockito.Mockito;
+
+import android.content.Context;
 import android.test.AndroidTestCase;
 
 public class EulaTest extends AndroidTestCase {
 	
+	private Context mockContext;
+	
 	public EulaTest() {
 		super();
+		mockContext = Mockito.mock(Context.class);
 	}
 	
 	public void testShowEula()	{
-		Eula.showEula(getContext());
+		Eula.showEula(mockContext);
 	}
-
 }
