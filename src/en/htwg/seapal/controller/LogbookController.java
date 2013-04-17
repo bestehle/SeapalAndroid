9083058b8c2d66964549f18f7ece6389254018a7
chbraun <chbraun@htwg-konstanz.de>
2013-04-17 20:52:56 +0200

    add Javadocs in LogbookController

--- src/en/htwg/seapal/controller/LogbookController.java
+++ src/en/htwg/seapal/controller/LogbookController.java
@@ -4,10 +4,14 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import android.content.Context;
-
 import en.htwg.seapal.model.models.Boat;
 import en.htwg.seapal.model.tables.TableBoat;
 
+/**
+ * ControllerClass for Logbook to add and get Boats from the Table
+ * @author 
+ * 
+ */
 public class LogbookController {
 
 	private Context activity = null;
@@ -16,6 +20,10 @@ public class LogbookController {
 		this.activity = activity;
 	}
 	
+	/**
+	 * Get all Boats from Table
+	 * @return List of Boats
+	 */
 	public List<Boat> getBoats() {
 		TableBoat tableBoat = new TableBoat(activity);
 		List<Boat> list = null;
@@ -44,6 +52,11 @@ public class LogbookController {
 		return list;
 	}
 	
+	
+	/**
+	 * Add a Boat in the Table
+	 * @param boat The Boat to add
+	 */
 	public void addBoat(Boat boat) {
 		TableBoat tableBoat = new TableBoat(activity);
 		try {
@@ -60,3 +73,5 @@ public class LogbookController {
 		}
 	}
 }
+
+
